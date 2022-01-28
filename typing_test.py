cbdd9c09acdb604f702f4c95d20112ef2f92dca0
TheHCL <daniel8411222@gmail.com>
2022-01-28 14:18:50 +0800

    avoid nonascii char

--- typing_test.py
+++ typing_test.py
@@ -1,23 +1,25 @@
 import random,os,time,requests,json
 
 url="https://random-words-api.vercel.app/word"
-
+os.system("cls")
+count=0
 while True:
 
 	x=requests.get(url)
 	x=json.loads(x.text)
 	x=x[0]['word'].lower()
-	
-
-	print(x)
-	s_time=time.time()
-
-	while input() !=x:
+	if x.isascii() == True:
+		count+=1
+		print("\n"+x)
 		s_time=time.time()
+
+		while input() !=x:
+			s_time=time.time()
+			os.system("cls")
+			print(x)
+
+
+		x=""
 		os.system("cls")
-		print(x)
-
-
-	x=""
-	os.system("cls")
-	print("Spent : "+str(60/(time.time()-s_time))+" wpm.")
+		print("Count: "+str(count)+"\n")
+		print("Spent : "+str(60/(time.time()-s_time))+" wpm.")
